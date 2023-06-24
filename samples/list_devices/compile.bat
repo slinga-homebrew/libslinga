3a6578d218f50a98120b1b25ecfc9e7abe07535b
slinga-homebrew <58894244+slinga-homebrew@users.noreply.github.com>
2023-06-24 00:35:43 -0400

    Delete samples/list_devices directory

--- samples/list_devices/compile.bat
+++
@@ -1,4 +0,0 @@
-@ECHO Off
-SET COMPILER_DIR=..\..\Compiler
-SET PATH=%COMPILER_DIR%\WINDOWS\Other Utilities;%COMPILER_DIR%\WINDOWS\bin;%PATH%
-make re
--- samples/list_devices/run_with_kronos.bat
+++
@@ -1,9 +0,0 @@
-@ECHO Off
-SET EMULATOR_DIR=..\..\Emulators
-
-if exist game.iso (
-echo Press F1 inside Kronos to run the game
-"%EMULATOR_DIR%\kronos\kronos.exe" -i game.iso
-) else (
-echo Please compile first !
-)
